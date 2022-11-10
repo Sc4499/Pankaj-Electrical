56628dc209b40b15b096780a4fd054b982fb7610
Sc4499 <schaudhary100111@gmail.com>
2022-11-10 17:53:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Login from './Login';
 
 const App = () => {
+  const[name,setname]=useState("rohan");
+  function clickhandler(namedata){
+setname(namedata)
+  }
   return (
     <div>
-      <Login/>
+      <h1>welcome:{name}</h1>
+      <Login senddata={clickhandler}/>
     </div>
   )
 }
--- src/Login.js
+++ src/Login.js
@@ -1,55 +1,11 @@
+import React from 'react'
 
-import React, { useState } from 'react';
-import "./Login.css";
-
-const Login = () => {
-    const[email,setEmail]=useState();
-    const[password,setPassword]=useState();
-    const[name,setame]=useState();
-    const[allEntry,setAllEntry] = useState([]);
-
-    function submitHandler(e){
-e.preventDefault();
-const newEntry = {name : name , email : email, password:password};
-setAllEntry([...allEntry,newEntry]);
-console.log(allEntry);
-    }
-
-
+const Login = (prop) => {
+  const namedata = "shailendra";
   return (
-    <>
-    <form onSubmit={submitHandler}>
-        <div>
-        <div className="mb-3">
-      <label htmlFor="exampleInputname" className="form-label">Name</label>
-      <input type="text" name={name} className="form-control" id="exampleInputname" value={name} onChange={(e)=>setame(e.target.value)} aria-describedby="emailHelp"/>
+    <div>
+      <button onClick={()=>prop.senddata(namedata)}>click me</button>
     </div>
-        <div className="mb-3">
-      <label htmlfor="exampleInputEmail1" className="form-label">Email address</label>
-      <input type="email" className="form-control" value={email}  onChange={(e)=>setEmail(e.target.value)} id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    </div>
-    <div className="mb-3">
-      <label htmlfor="exampleInputPassword1" className="form-label">Password</label>
-      <input type="password" className="form-control"  value={password} onChange={(e)=>setPassword(e.target.value)}id="exampleInputPassword1"/>
-    </div>
-    <button type="submit" className="btn btn-dark w-100">Submit</button>
-        </div>
-   
-  </form>
- 
-    {allEntry.map(item =>{
-      return(
-      
-         <div className='showdata'>
-        <div>{item.name}</div>
-        <div>{item.email}</div>
-        <div>{item.password}</div>
-        </div>
-      )
-    })}
-   
-  
-  </>
   )
 }
 
